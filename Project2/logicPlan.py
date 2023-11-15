96fc6b3da9e0afcdef471d39fe124ca49b6b41f9
hieu.nh <hieu.nh@teko.vn>
2023-11-15 13:20:50 +0700

    Q8

--- Project2/logicPlan.py
+++ Project2/logicPlan.py
@@ -665,11 +665,31 @@ def slam(problem, agent) -> Generator:
             outer_wall_sent.append(PropSymbolExpr(wall_str, x, y))
     KB.append(conjoin(outer_wall_sent))
 
-    "*** BEGIN YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    KB.append(PropSymbolExpr(pacman_str, pac_x_0, pac_y_0, time = 0))
+    KB.append(~PropSymbolExpr(wall_str, pac_x_0, pac_y_0))
+    known_map[pac_x_0][pac_y_0] = 0
 
     for t in range(agent.num_timesteps):
-        "*** END YOUR CODE HERE ***"
+        KB.append(pacphysicsAxioms(t, all_coords, non_outer_wall_coords, known_map, SLAMSensorAxioms, SLAMSuccessorAxioms))
+        KB.append(PropSymbolExpr(agent.actions[t], time = t))
+        KB.append(numAdjWallsPerceptRules(t, agent.getPercepts()))
+
+        for x, y in non_outer_wall_coords:
+            if entails(conjoin(KB), ~PropSymbolExpr(wall_str, x, y)):
+                KB.append(~PropSymbolExpr(wall_str, x, y))
+                known_map[x][y] = 0
+            if entails(conjoin(KB), PropSymbolExpr(wall_str, x, y)):
+                KB.append(PropSymbolExpr(wall_str, x, y))
+                known_map[x][y] = 1
+
+        possible_locations = []
+        for x, y in non_outer_wall_coords:
+            possible_model = findModel(conjoin(KB) & PropSymbolExpr(pacman_str, x, y, time=t))
+            if possible_model:
+                possible_locations.append((x, y))
+
+        agent.moveToNextState(agent.actions[t])
+
         yield (known_map, possible_locations)
 
 
@@ -684,7 +704,6 @@ sys.setrecursionlimit(100000)
 #______________________________________________________________________________
 # Important expression generating functions, useful to read for understanding of this project.
 
-
 def sensorAxioms(t: int, non_outer_wall_coords: List[Tuple[int, int]]) -> Expr:
     all_percept_exprs = []
     combo_var_def_exprs = []
@@ -695,7 +714,7 @@ def sensorAxioms(t: int, non_outer_wall_coords: List[Tuple[int, int]]) -> Expr:
             combo_var = PropSymbolExpr(pacman_wall_str, x, y, x + dx, y + dy, time=t)
             percept_exprs.append(combo_var)
             combo_var_def_exprs.append(combo_var % (
-                PropSymbolExpr(pacman_str, x, y, time=t) & PropSymbolExpr(wall_str, x + dx, y + dy)))
+                    PropSymbolExpr(pacman_str, x, y, time=t) & PropSymbolExpr(wall_str, x + dx, y + dy)))
 
         percept_unit_clause = PropSymbolExpr(blocked_str_map[direction], time = t)
         all_percept_exprs.append(percept_unit_clause % disjoin(percept_exprs))
